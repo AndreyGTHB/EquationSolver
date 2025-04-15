33defd59b3ebef0e1152d53dd0126da46931291e
grigory <grigory@goodnotesapp.com>
2025-04-15 22:26:55 +0100

    console colors: code cleanup

--- src/console/Clr.kt
+++ src/console/Clr.kt
@@ -6,11 +6,11 @@ object Clr {
 
     const val RC = "${esc}0m" // Reset foreground and background colors. --> $gnc.gnc.console.console.RC
 
-    const val R = "${esc}7m"  // Invert foreground to background. --> $gnc.gnc.console.console.R
-    const val U = "${esc}4m"  // Underline. --> $gnc.gnc.console.console.U
-    const val B = "${esc}1m"  // Bold. --> $gnc.gnc.console.console.B
-    const val I = "${esc}3m"  // Italic. --> $gnc.gnc.console.console.I
-    const val S = "${esc}9m"  // Strikethrough the text. --> $gnc.gnc.console.console.S
+    const val R = "${esc}7m"  // Invert foreground to background
+    const val U = "${esc}4m"  // Underline
+    const val B = "${esc}1m"  // Bold
+    const val I = "${esc}3m"  // Italic
+    const val S = "${esc}9m"  // Strikethrough the text
 
     const val WHITE = "${esc}38;5;7m"
     const val RED = "${esc}38;5;1m"
@@ -21,17 +21,15 @@ object Clr {
     const val ORANGE = "${esc}38;5;208m"
 
     // 256 Colors
-    fun fg(n: Int) = "${esc}38;5;${n}m" // Set a foreground color. --> ${gnc.gnc.console.console.fg(40)} //Sets a green color.
-    fun bg(n: Int) = "${esc}48;5;${n}m" // Set a background color. --> ${gnc.gnc.console.console.bg(196)} //Sets a red color.
+    fun fg(n: Int) = "${esc}38;5;${n}m" // Set a foreground color.
+    fun bg(n: Int) = "${esc}48;5;${n}m" // Set a background color.
 
     // RGB Colors
-    fun rgbFg(r: Int, g: Int, b: Int) = "${esc}38;2;$r;$g;${b}m" // Set a RGB foreground color. --> ${gnc.gnc.console.console.rgbfg(0,255,0)} //Sets a green color.
-    fun rgbBg(r: Int, g: Int, b: Int) = "${esc}48;2;$r;$g;${b}m" // Set a RGB background color. --> ${gnc.gnc.console.console.rgbbg(255,0,0)} //Sets a red color.
+    fun rgbFg(r: Int, g: Int, b: Int) = "${esc}38;2;$r;$g;${b}m" // Set a RGB foreground color.
+    fun rgbBg(r: Int, g: Int, b: Int) = "${esc}48;2;$r;$g;${b}m" // Set a RGB background color.
 
     // predefined "nice" colors
     val palette = arrayOf(120, 229, 135, 4, 5, 6, 249, 179, 227)
-    fun std256Index(index: Int): Int = palette.getOrElse(index) { 103 + index }
-    fun std(index: Int): Int = palette.getOrElse(index) { 103 + index }
 
     fun String.highlight(baseColor: String? = null): String {
         val clr = baseColor ?: RC
